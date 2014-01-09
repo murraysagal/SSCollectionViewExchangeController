a81e005ae942d8321ef1edd33f14decf8cb4a065
murraysagal <murraysagal@mac.com>
2014-01-09 17:59:40 +0100

    Added SSCollectionViewExchangeController class.

---
+++ Exchanger/SSCollectionViewExchangeController.h
@@ -0,0 +1,13 @@
+//
+//  SSCollectionViewExchangeController.h
+//  Exchanger
+//
+//  Created by Murray Sagal on 1/9/2014.
+//  Copyright (c) 2014 Murray Sagal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSCollectionViewExchangeController : UICollectionViewController
+
+@end
---
+++ Exchanger/SSCollectionViewExchangeController.m
@@ -0,0 +1,38 @@
+//
+//  SSCollectionViewExchangeController.m
+//  Exchanger
+//
+//  Created by Murray Sagal on 1/9/2014.
+//  Copyright (c) 2014 Murray Sagal. All rights reserved.
+//
+
+#import "SSCollectionViewExchangeController.h"
+
+@interface SSCollectionViewExchangeController ()
+
+@end
+
+@implementation SSCollectionViewExchangeController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
